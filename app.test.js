59da37778d72e49728ad703084e22c077da78cb5
rapipp-xyra <anandradanisyahrafif@gmail.com>
2026-07-05 01:42:03 +0700

    Test: sengaja untuk dibuat test gagal untuk verifikasi pipeline github ya

--- app.test.js
+++ app.test.js
@@ -1,8 +1,4 @@
 test('pesan hello devops benar', () => {
   const message = 'Hello DevOps!';
-  expect(message).toBe('Hello DevOps!');
-});
-
-test('operasi matematika sederhana', () => {
-  expect(2 + 2).toBe(4);
+  expect(message).toBe('Salah Sengaja);
 });
